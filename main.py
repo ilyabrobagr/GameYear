083aadffd80d6bcec929a91cdd3906dac59859b5
Bagrov Ilya <ilyabagrov06@mail.ru>
2021-03-14 19:23:18 +0300

    added bg

--- main.py
+++ main.py
@@ -1,44 +1,27 @@
 import pygame
 
+class Object(pygame.sprite.Sprite):
+    def ___init___(self, img, x, y, speed):
+        pygame.sprite.Sprite.___init___(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+
 window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Игра года")
+
+
+bg = pygame.transform.scale(pygame.image.load("images/bg.png"), (800, 600))
+
 run = True
 
 while run:
+    window.blit(bg, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
