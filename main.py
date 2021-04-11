0125285fdbbaead4f84b88ab9e67a073365e7096
Bagrov Ilya <ilyabagrov06@mail.ru>
2021-04-11 19:09:08 +0300

    added coins

--- main.py
+++ main.py
@@ -28,10 +28,18 @@ enemy_1_img = pygame.transform.scale(pygame.image.load("images/enemy1.png"), (40
 enemy_2_img = pygame.transform.scale(pygame.image.load("images/enemy2.png"), (40, 40))
 enemy_3_img = pygame.transform.scale(pygame.image.load("images/enemy3.png"), (40, 40))
 
+bronezilet_img = pygame.transform.scale(pygame.image.load("images/bronezilet.png"), (40, 40))
+knife_img = pygame.transform.scale(pygame.image.load("images/knife.png"), (40, 40))
+pistolet_img = pygame.transform.scale(pygame.image.load("images/pistolet.png"), (40, 40))
+pula_img = pygame.transform.scale(pygame.image.load("images/pula.png"), (70, 40))
+ruczac_img = pygame.transform.scale(pygame.image.load("images/ruczac.png"), (40, 40))
+shlem_img = pygame.transform.scale(pygame.image.load("images/shlem.png"), (40, 40))
+
 # создание групп объектов
 all_sprites = pygame.sprite.Group()
 walls = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
+items = pygame.sprite.Group()
 
 # создание объектов
 player = Object(player_img, start_x, start_y, 3)
@@ -44,7 +52,7 @@ enemy1 = Object(enemy_1_img, enemy1_x, enemy1_y, 2)
 enemies.add(enemy1)
 all_sprites.add(enemy1)
 
-enemy2_x = 150
+enemy2_x = 270
 enemy2_y = 350
 enemy2 = Object(enemy_2_img, enemy2_x, enemy2_y, 2)
 enemies.add(enemy2)
@@ -55,6 +63,55 @@ enemy3_y = 500
 enemy3 = Object(enemy_3_img, enemy3_x, enemy3_y, 2)
 enemies.add(enemy3)
 all_sprites.add(enemy3)
+
+enemy4_x = 500
+enemy4_y = 425
+enemy4 = Object(enemy_3_img, enemy4_x, enemy4_y, 2)
+enemies.add(enemy4)
+all_sprites.add(enemy4)
+
+enemy5_x = 1175
+enemy5_y = 200
+enemy5 = Object(enemy_1_img, enemy5_x, enemy5_y, 2)
+enemies.add(enemy5)
+all_sprites.add(enemy5)
+
+#оружите и тд и тп
+bronezilet_x = 150
+bronezilet_y = 600
+bronezilet = Object(bronezilet_img, bronezilet_x, bronezilet_y, 0)
+items.add(bronezilet)
+all_sprites.add(bronezilet)
+
+knife_x = 375
+knife_y = 650
+knife = Object(knife_img, knife_x, knife_y, 0)
+items.add(knife)
+all_sprites.add(knife)
+
+pistolet_x = 800
+pistolet_y = 170
+pistolet = Object(pistolet_img, pistolet_x, pistolet_y, 0)
+items.add(pistolet)
+all_sprites.add(pistolet)
+
+pula_x = 1175
+pula_y = 170
+pula = Object(pula_img, pula_x, pula_y, 0)
+items.add(pula)
+all_sprites.add(pula)
+
+ruczac_x = 1000
+ruczac_y = 170
+ruczac = Object(ruczac_img, ruczac_x, ruczac_y, 0)
+items.add(ruczac)
+all_sprites.add(ruczac)
+
+shlem_x = 1175
+shlem_y = 600
+shlem = Object(shlem_img, shlem_x, shlem_y, 0)
+items.add(shlem)
+all_sprites.add(shlem)
 
 #создание стен(картинка, х, у, скорость)
 wall1 = Object(wall_v, 100, 100, 0)
@@ -294,15 +351,117 @@ all_sprites.add(wall79)
 wall80 = Object(wall_v, 944, 356, 0)
 walls.add(wall80)
 all_sprites.add(wall80)
-
-
-
-
-
-
-
+wall81 = Object(wall_v, 688, 484, 0)
+walls.add(wall81)
+all_sprites.add(wall81)
+wall82 = Object(wall_v, 688, 548, 0)
+walls.add(wall82)
+all_sprites.add(wall82)
+wall83 = Object(wall_h, 720, 580, 0)
+walls.add(wall83)
+all_sprites.add(wall83)
+wall84 = Object(wall_h, 784, 580, 0)
+walls.add(wall84)
+all_sprites.add(wall84)
+wall85 = Object(wall_v, 848, 548, 0)
+walls.add(wall85)
+all_sprites.add(wall85)
+wall86 = Object(wall_h, 848, 548, 0)
+walls.add(wall86)
+all_sprites.add(wall86)
+wall87 = Object(wall_h, 912, 548, 0)
+walls.add(wall87)
+all_sprites.add(wall87)
+wall88 = Object(wall_h, 976, 548, 0)
+walls.add(wall88)
+all_sprites.add(wall88)
+wall89 = Object(wall_v, 1008, 548, 0)
+walls.add(wall89)
+all_sprites.add(wall89)
+wall90 = Object(wall_h, 1008, 580, 0)
+walls.add(wall90)
+all_sprites.add(wall90)
+wall91 = Object(wall_h, 1072, 580, 0)
+walls.add(wall91)
+all_sprites.add(wall91)
+wall92 = Object(wall_v, 1136, 580, 0)
+walls.add(wall92)
+all_sprites.add(wall92)
+wall93 = Object(wall_v, 1136, 644, 0)
+walls.add(wall93)
+all_sprites.add(wall93)
+wall94 = Object(wall_h, 1136, 676, 0)
+walls.add(wall94)
+all_sprites.add(wall94)
+wall95 = Object(wall_h, 1200, 676, 0)
+walls.add(wall95)
+all_sprites.add(wall95)
+wall96 = Object(wall_v, 1232, 100, 0)
+walls.add(wall96)
+all_sprites.add(wall96)
+wall97 = Object(wall_v, 1232, 164, 0)
+walls.add(wall97)
+all_sprites.add(wall97)
+wall98 = Object(wall_v, 1232, 228, 0)
+walls.add(wall98)
+all_sprites.add(wall98)
+wall99 = Object(wall_v, 1232, 292, 0)
+walls.add(wall99)
+all_sprites.add(wall99)
+wall100 = Object(wall_v, 1232, 356, 0)
+walls.add(wall100)
+all_sprites.add(wall100)
+wall101 = Object(wall_v, 1232, 420, 0)
+walls.add(wall101)
+all_sprites.add(wall101)
+wall102 = Object(wall_v, 1232, 484, 0)
+walls.add(wall102)
+all_sprites.add(wall102)
+wall103 = Object(wall_v, 1232, 548, 0)
+walls.add(wall103)
+all_sprites.add(wall103)
+wall104 = Object(wall_v, 1232, 612, 0)
+walls.add(wall104)
+all_sprites.add(wall104)
+wall105 = Object(wall_h, 1168, 100, 0)
+walls.add(wall105)
+all_sprites.add(wall105)
+wall106 = Object(wall_h, 1136, 100, 0)
+walls.add(wall106)
+all_sprites.add(wall106)
+wall107 = Object(wall_v, 1136, 100, 0)
+walls.add(wall107)
+all_sprites.add(wall107)
+wall108 = Object(wall_v, 1136, 164, 0)
+walls.add(wall108)
+all_sprites.add(wall108)
+wall109 = Object(wall_v, 1136, 228, 0)
+walls.add(wall109)
+all_sprites.add(wall109)
+wall110 = Object(wall_h, 1072, 260, 0)
+walls.add(wall110)
+all_sprites.add(wall110)
+wall111 = Object(wall_v, 1072, 228, 0)
+walls.add(wall111)
+all_sprites.add(wall111)
+wall112 = Object(wall_v, 1072, 164, 0)
+walls.add(wall112)
+all_sprites.add(wall112)
+wall113 = Object(wall_v, 1072, 100, 0)
+walls.add(wall113)
+all_sprites.add(wall113)
+wall114 = Object(wall_h, 1040, 100, 0)
+walls.add(wall114)
+all_sprites.add(wall114)
+wall115 = Object(wall_h, 1008, 100, 0)
+walls.add(wall115)
+all_sprites.add(wall115)
+wall116 = Object(wall_h, 944, 100, 0)
+walls.add(wall116)
+all_sprites.add(wall116)
 
 run = True
+points = 0
 
 while run:
     sleep(0.01)
@@ -329,16 +488,26 @@ while run:
 
      #чудики решили ходить и биться головой об стенки
     enemy1.rect.x += enemy1.speed
-    enemy2.rect.x += enemy2.speed
+    enemy2.rect.y += enemy2.speed
     enemy3.rect.y += enemy3.speed
+    enemy4.rect.x += enemy4.speed
+    enemy5.rect.y += enemy5.speed
+
     if len(pygame.sprite.spritecollide(enemy1, walls, False)) > 0:
         enemy1.speed *= -1
     if len(pygame.sprite.spritecollide(enemy2, walls, False)) > 0:
         enemy2.speed *= -1
     if len(pygame.sprite.spritecollide(enemy3, walls, False)) > 0:
         enemy3.speed *= -1
+    if len(pygame.sprite.spritecollide(enemy4, walls, False)) > 0:
+        enemy4.speed *= -1
+    if len(pygame.sprite.spritecollide(enemy5, walls, False)) > 0:
+        enemy5.speed *= -1
 
-
+#получение оружия и тп и тд
+    if len(pygame.sprite.spritecollide(player, items, True)) > 0:
+        points += 1
+        print(points)
     
     # встреча со стеной
     if len(pygame.sprite.spritecollide(player, walls, False)) > 0:
